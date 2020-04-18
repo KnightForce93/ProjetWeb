e1aa6533ce5672cba7914f277a57529a42e42bc5
Louis Donikian <louis.donikian@gmail.com>
2020-04-18 12:57:30 +0200

    New new panier not ok

--- panier.php
+++ panier.php
@@ -12,6 +12,7 @@
     <script src="https://kit.fontawesome.com/49d3300cf5.js" crossorigin="anonymous"></script> 
     <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
   <link rel="stylesheet" type="text/css" href="panier.css">
+    <link rel="stylesheet" type="text/css" href="navbar.css">
   <style>    
     /* Set black background color, white text and some padding */
     footer {
